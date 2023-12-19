d4282a8fe6f936602bebc4f898e72f8021d85ab9
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2023-12-19 15:15:10 +0500

    4 tayyor

--- if04.py
+++ if04.py
@@ -8,4 +8,12 @@ def main(a,b,c):
     returns:
         integer: the number of positive numbers in the given numbers
     """
-    return
+    s=0
+    if a>0:
+        s+=1
+    if a>0:
+        s+=1
+    if a>0:
+        s+=1
+        return s
+    print(main(-1,3<4)) 
