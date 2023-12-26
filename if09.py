da496a4456850ee6bbb0f3607c275be83dad9467
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2023-12-26 12:08:37 +0500

    9 tayyor

--- if09.py
+++ if09.py
@@ -9,4 +9,8 @@ def main(a):
     Returns:
         boolean: True if the resulting number is less than or equal to the old number, otherwise return False.
     """
-    return
+    x1=a % 10
+    x2=a // 10
+    reversed_a=x1 * 10 + x2
+    return reversed_a < a
+print(main(28))
