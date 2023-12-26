55839c4b2b690fbd2d0dafae7972fae8e16db62e
QAmirkhan1177 <amirxonqarshiyev@gmail.com>
2023-12-26 11:56:49 +0500

    8 tayyor

--- if08.py
+++ if08.py
@@ -11,4 +11,14 @@ def main(a):
     Returns:
         string: the message to print
     """
-    return
+    result=''
+    if a>9 and a<100 and a%2 ==1:
+        result="two-digit odd number"
+    if a>9 and a<100 and a%2 ==0:
+        result="two-digit even number"
+    if a>99 and a<1000 and a%2 ==1:
+        result="three-digit odd number"
+    if a>99 and a<1000 and a%2 ==0:
+        result="three-digit even number"  
+    return result
+print(main(127)) 
